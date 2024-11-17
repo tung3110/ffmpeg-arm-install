c384ab81c3eb408db9a606ca9d933ca0500c5558
tung3110 <tungnh@outlook.com>
2024-11-18 01:39:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ cd ffmpeg
 
 cp libffmpeg/libx264.so.155  /usr/lib/aarch64-linux-gnu/libx264.so.155
 
-cp libffmpeg/libsrt.so.1  /usr/lib/x86_64-linux-gnu/libsrt.so.1
+cp libffmpeg/libsrt.so.1  /usr/lib/aarch64-linux-gnu/libsrt.so.1
 
-cp libffmpeg/libcrypto.so.1.1 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1
+cp libffmpeg/libcrypto.so.1.1 /usr/lib/aarch64-linux-gnu/libcrypto.so.1.1
 
 cp ffmpeg /usr/bin
 
