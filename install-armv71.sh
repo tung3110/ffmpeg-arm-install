ed5c0124a475a5ca35711fc639cb6462f5bcaa6b
tung3110 <tungnh@outlook.com>
2024-11-22 09:32:45 +0700

    Create install-armv71.sh

---
+++ install-armv71.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt -y update && apt -y install  libsdl2-dev libva-dev ffmpeg libavformat-dev libavcodec-dev libswscale-dev zip wget libxcb-shm0-dev libxcb-xfixes0-dev
+
+cd ffmpeg-arm-install
+
+unzip ffmpeg-armv71.zip
+
+cd ffmpeg
+
+cp libffmpeg/libx264.so.155  /usr/lib/arm-linux-gnueabihf/libx264.so.155
+
+cp libffmpeg/libsrt.so.1  /usr/lib/arm-linux-gnueabihf/libsrt.so.1
+
+cp libffmpeg/libcrypto.so.1.1 /usr/lib/arm-linux-gnueabihf/libcrypto.so.1.1
+
+cp ffmpeg /usr/bin
+
+cp ffplay /usr/bin
